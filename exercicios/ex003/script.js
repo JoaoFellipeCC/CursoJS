34b3ad0618c9c4ac366c6ab0d4e3780022103ed9
Joao-Fellipe <jfellipe1789@gmail.com>
2021-11-08 22:49:21 -0200

    Ajustes ex003

--- exercicios/ex003/script.js
+++ exercicios/ex003/script.js
@@ -1,14 +1,14 @@
 function contar(){
     var starttxt = window.document.getElementById('start')
-    var start = Number(starttxt.value)
     var endtxt = window.document.getElementById('end')
-    var end = Number(endtxt.value)
     var conttxt = window.document.getElementById('cont')
-    var cont = Number(conttxt.value)
     var res = window.document.getElementById('res')
-    if(start == "" || end == ""){
+    if(starttxt.value.length == 0 || endtxt.value.length == 0 || conttxt.value.length == 0){
         res.innerHTML = `<p>Impossível contar!</p>`
     }else{
+        var start = Number(starttxt.value)
+        var end = Number(endtxt.value)
+        var cont = Number(conttxt.value)
         res.innerHTML = `Contando:<br>`
         if(!cont){
             window.alert('Passo invalido! Considerando PASSO 1.')
